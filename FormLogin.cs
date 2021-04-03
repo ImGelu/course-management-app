c4e227b0d1a5009c66367f7cf48daf46e9e8532f
Gelu Ungur <ImGelu@gmail.com>
2021-04-03 18:40:33 +0300

    Added Service Test + DB Tables

---
+++ FormLogin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proiect
+{
+    public partial class FormLogin : Form
+    {
+        public static CoursesWebServiceReference.CoursesWebService webService = new CoursesWebServiceReference.CoursesWebService();
+
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String helloWorld = webService.HelloWorld();
+            textBox1.Text = helloWorld;
+        }
+    }
+}
